896f1c84f83d2cecd501b025bf43ca69f8932916
Kryspinn <33183934+Kryspinn@users.noreply.github.com>
2018-03-27 13:50:09 +0200

    03-27

--- app/src/main/java/fq/fuqueue/MainActivity.java
+++ app/src/main/java/fq/fuqueue/MainActivity.java
@@ -1,5 +1,6 @@
 package fq.fuqueue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread = new Thread(){
+          public void run(){
+              try {
+                  sleep(2000);
+              } catch (InterruptedException e1) {
+                  e1.printStackTrace();
+              }finally {
+                  Intent intent = new Intent(".chosen");
+                  startActivity(intent);
+              }
+          }
+        };
+        thread.start();
     }
 }
---
+++ app/src/main/java/fq/fuqueue/chosen.java
@@ -0,0 +1,13 @@
+package fq.fuqueue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chosen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chosen);
+    }
+}
