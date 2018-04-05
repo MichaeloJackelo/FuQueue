a628015948dacecbe0a7732f8acf28d429e70d34
Kryspinn <33183934+Kryspinn@users.noreply.github.com>
2018-04-05 12:47:14 +0200

    testkrystian

--- app/src/main/java/fq/fuqueue/MainActivity.java
+++ app/src/main/java/fq/fuqueue/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        thread.start(); //test43srrfgtfgfdsg
+        thread.start(); //testkrystiangłówny
     }
 }
