3953a8a6f494f1c2ab93facfc0cc90b88df3259a
Michal Miotk <michalmiotk95@gmail.com>
2018-04-05 11:35:46 +0200

    test3
    This reverts commit c5097b679f9532a77a585239c11b3efdc01becf4.

--- app/src/main/java/fq/fuqueue/chosen.java
+++ app/src/main/java/fq/fuqueue/chosen.java
@@ -2,12 +2,29 @@ package fq.fuqueue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 public class chosen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chosen);
+        Button clickButton = (Button) findViewById(R.id.button_change);
+        clickButton.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                next_page(v);
+            }
+        });
+    }
+    public void next_page(View v)
+    {
+        Intent intent = new Intent(this, ActiveShoppingList.class);
+        startActivity(intent);
     }
 }
