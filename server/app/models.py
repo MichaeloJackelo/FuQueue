0ad93db22cb50b9c96d0f9a261efdf8e027513f9
Michal Jackowski <michal.jackowski1@gmail.com>
2018-05-16 00:05:44 +0200

    Product post with json mimetype handling.

--- server/app/models.py
+++ server/app/models.py
@@ -3,15 +3,20 @@ from app import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), index=True, unique=True)
+    name = db.Column(db.String(128))
     prize = db.Column(db.Float)
     file_name = db.Column(db.String(128))
-    barcode = db.Column(db.Integer)
+    barcode = db.Column(db.Integer, unique=True)
     description = db.Column(db.String(256))
 
     def to_dict(self):
         return {"id": self.id, "barcode": self.barcode, "name": self.name,
                 "prize": self.prize, "file_name": self.file_name, "description": self.description}
+
+    def from_dict(self, data):
+        for field in ['id', 'barcode', 'name', 'prize' , 'file_name', 'description']:
+            if field in data:
+                setattr(self, field, data[field])
 
     def __repr__(self):
         return '<Product {}>'.format(self.name)
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,11 +1,27 @@
-from flask import url_for, redirect, jsonify, render_template
+from flask import url_for, redirect, jsonify, request, render_template
 from app import app, models, forms, db
+from werkzeug.http import HTTP_STATUS_CODES
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
+
+def wants_json_response():
+    return request.accept_mimetypes['application/json'] >= \
+        request.accept_mimetypes['text/html']
+
+def bad_request(message):
+    return error_response(400, message)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return redirect(url_for('products'))
-
 
 @app.route('/products/')
 def products():
@@ -20,17 +36,38 @@ def product(barcode):
     return jsonify(product.to_dict())
 
 
-@app.route('/products/add/', methods=['GET', 'POST'])
+@app.route('/products/add/', methods=['POST'])
 def add_product():
-    form = forms.ProductForm()
-    if form.validate_on_submit():
-        p = models.Product(name=form.name.data,
-                           prize=form.prize.data,
-                           file_name=form.file_name.data,
-                           barcode=form.barcode.data,
-                           description=form.description.data)
-        db.session.add(p)
-        db.session.commit()
-        return redirect(url_for('products'))
-    return render_template('add_product.html', title='Sign In', form=form)
+    if not wants_json_response():
+        form = forms.ProductForm()
+        if form.validate_on_submit():
+            p = models.Product(name=form.name.data,
+                               prize=form.prize.data,
+                               file_name=form.file_name.data,
+                               barcode=form.barcode.data,
+                               description=form.description.data)
+            db.session.add(p)
+            db.session.commit()
+            return redirect(url_for('products'))
+        return render_template('add_product.html', title='Sign In', form=form)
 
+    data = request.get_json() or {}
+    if "name" not in data or "prize" not in data or "file_name" not in data or "barcode" \
+        not in data or "description" not in data:
+        return bad_request("missing field")
+    p = models.Product()
+    p.from_dict(data)
+    db.session.add(p)
+    db.session.commit()
+    response = jsonify(p.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('products', barcode=p.barcode)
+    return response
+
+
+@app.route('/products/add/', methods=['GET'])
+def render_add_product():
+    if not wants_json_response():
+        form = forms.ProductForm()
+        return render_template('add_product.html', title='Sign In', form=form)
+    return bad_request("bad request")
